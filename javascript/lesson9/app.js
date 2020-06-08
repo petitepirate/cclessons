aa971654a17d72e25c9d4107f62d908e74048b2c
megan <msmeganmcmanus@gmail.com>
2020-06-08 15:13:12 -0700

    update lesson 9

---
+++ javascript/lesson9/app.js
@@ -0,0 +1,8 @@
+let wordCount = 1;
+
+let tool = 'apple';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
