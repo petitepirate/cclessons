f2b4395b15f075859a24aa073a68bdfb3236deac
megan <msmeganmcmanus@gmail.com>
2020-06-09 15:09:01 -0700

    add lesson 6

---
+++ javascript/lesson6/app.js
@@ -0,0 +1,16 @@
+let sale = true;
+
+sale = true;
+
+if (sale) {
+	console.log('Time to buy!');
+} else {
+	console.log('Time to wait for a sale.');
+}
+
+let hungerLevel = 3;
+if (hungerLevel >= 7) {
+	console.log('Time to eat!');
+} else {
+	console.log('We can eat later!');
+}
